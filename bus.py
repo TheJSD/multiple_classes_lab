be525b0704687202c2d6f6553761a0420c40ea1f
James Dickson <jamesstuartdickson@gmail.com>
2023-07-11 12:38:37 +0100

    fixed a typo in pick up method and added drop off method

--- bus.py
+++ bus.py
@@ -10,5 +10,8 @@ class Bus:
     def passenger_count(self):
         return len(self.passengers)
 
-    def pick_up(self, passanger):
-        self.passengers.append(passanger)
+    def pick_up(self, passenger):
+        self.passengers.append(passenger)
+
+    def drop_off(self, passenger):
+        self.passengers.remove(passenger)
